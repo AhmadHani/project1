3e699ee65dac3a2c691e18868dceab74b0562dd6
AhmadHani <dr.dp2007@gmail.com>
2016-06-02 17:49:16 +0300

    First commit

---
+++ search.php
@@ -0,0 +1,24 @@
+<?php
+# Connect To the DataBase
+
+$connect = mysqli_connect("localhost","root","root","blogger");
+
+# VAR
+
+$txt_search = htmlspecialchars($_POST['text_search']);
+
+
+# Search in the DataBase
+
+$search_db = mysqli_query($connect,"SELECT * FROM users WHERE email LIKE '%$txt_search%'"); 
+
+// % = search 0,9 and a,z
+
+# For Loop all the DataBase
+
+
+while($fetch_search_db = mysqli_fetch_assoc($search_db)) {
+	
+echo 'email : '.$fetch_search_db['email'];	
+}
+?>
